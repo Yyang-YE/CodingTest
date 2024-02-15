6621d3b248769d8806e68b5e8abcb40fdb9e2756
Yyang-YE <149645095+Yyang-YE@users.noreply.github.com>
2024-02-15 23:44:15 +0900

    [level 1] Title: K번째수, Time: 0.30 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+
+        for (int i = 0; i < commands.length; i++) {
+            for (int j = commands[i][0]-1; j <= commands[i][1]-1; j++) {
+                temp.add(array[j]);
+            }
+            Collections.sort(temp);
+            answer[i] = temp.get(commands[i][2]-1);
+            temp.clear();
+        }
+        return answer;
+    }
+}
