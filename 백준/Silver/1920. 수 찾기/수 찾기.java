3ccfa41337fbcbe44c033b1cdf910a68672777d5
Yyang-YE <149645095+Yyang-YE@users.noreply.github.com>
2025-01-25 23:29:13 +0900

    [Silver IV] Title: 수 찾기, Time: 544 ms, Memory: 45376 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,47 +1,32 @@
-import java.util.*;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        Map<String, Integer> map = new HashMap<>();
 
-        // 첫번째 입력
-        int N = sc.nextInt();
-        int[] arr = new int[N];
-
-        for(int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            map.put(st.nextToken(), i);
         }
 
-        Arrays.sort(arr);
-
-        // 두번째 입력
-        int M = sc.nextInt();
-
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            int key = sc.nextInt();
-            sb.append(binarySearch(arr, key)).append('\n');
-        }
-
-        // 결과 출력
-        System.out.println(sb);
-    }
-    
-    private static int binarySearch(int[] arr, int key) {
-        int leftIndex = 0;
-        int rightIndex = arr.length - 1;
-
-        while (leftIndex <= rightIndex) {
-            int index = (leftIndex + rightIndex) / 2;
-
-            if (arr[index] == key) {
-                return 1;
-            } else if (arr[index] < key) {
-                leftIndex = index + 1;
+            if(map.containsKey(st.nextToken())) {
+                bw.write("1");
             } else {
-                rightIndex = index - 1;
+                bw.write("0");
             }
+            if(i != M - 1) bw.write("\n");
         }
-        return 0;
+        br.close();
+        bw.close();
     }
 }
