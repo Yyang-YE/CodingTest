5a82474af7036180583866c537c3245aa0ef9d00
Yyang-YE <149645095+Yyang-YE@users.noreply.github.com>
2025-02-06 11:01:42 +0900

    [level 1] Title: 폰켓몬, Time: 5.28 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int max = nums.length / 2;
+
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        return Math.min(max, set.size());
+    }
+}
