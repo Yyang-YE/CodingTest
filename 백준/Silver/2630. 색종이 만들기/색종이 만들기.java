ca2c752ec8896bda87cc28c17d54d8edbf4850d5
Yyang-YE <149645095+Yyang-YE@users.noreply.github.com>
2025-02-17 10:33:16 +0900

    [Silver II] Title: 색종이 만들기, Time: 136 ms, Memory: 15628 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,52 +1,52 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N;
     static int[][] paper;
-    static int whiteCount = 0;
-    static int blueCount = 0;
-    public static void main(String[] args) throws Exception {
+    static int whiteCount;
+    static int blueCount;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        
+        N = Integer.parseInt(br.readLine());
         paper = new int[N][N];
-
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 paper[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        checkPaper(N, 0, 0);
-        System.out.println(whiteCount + "\n" + blueCount);
+        
+        cutPaper(0, 0, N);
+
+        System.out.println(whiteCount);
+        System.out.println(blueCount);
     }
-    
-    public static void checkPaper(int N, int xStart, int yStart) {
-        boolean wFlag = false;
-        boolean bFlag = false;
 
-        for (int i = xStart; i < xStart + N; i++) {
-            for (int j = yStart; j < yStart + N; j++) {
-                if(paper[i][j] == 1) {
-                    bFlag = true;
-                } else {
-                    wFlag = true;
-                }
+    private static boolean checkColor(int sX, int sY, int len) {
+        int flag = paper[sX][sY];
+        for (int i = sX; i < sX + len; i++) {
+            for (int j = sY; j < sY + len; j++) {
+                if(paper[i][j] != flag) return false;
             }
-            if(bFlag && wFlag) break;
         }
+        return true;
+    }
 
-        // 둘 다 섞여있으면 4등분
-        if(bFlag && wFlag) {
-            int newN = N/2;
-            checkPaper(newN, xStart, yStart);
-            checkPaper(newN, xStart + newN, yStart);
-            checkPaper(newN, xStart, yStart + newN);
-            checkPaper(newN, xStart + newN, yStart + newN);
-        } else if(wFlag) { // white로만 이루어진 경우
-            whiteCount++;
-        } else if(bFlag) { // blue로만 이루어진 경우
-            blueCount++;
+    private static void cutPaper(int sX, int sY, int len) {
+        // 받은 종이가 다른 색이면 cut하기
+        if(!checkColor(sX, sY, len)) {
+            int newLen = len/2;
+            cutPaper(sX, sY, newLen);
+            cutPaper(sX+newLen, sY, newLen);
+            cutPaper(sX, sY+newLen, newLen);
+            cutPaper(sX+newLen, sY+newLen, newLen);
+        } else { // 전부 같은 색
+            if(paper[sX][sY] == 0) whiteCount++;
+            else blueCount++;
         }
     }
 }
