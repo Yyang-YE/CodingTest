2390742627c2fdd3280f462812d4de44dedfa8f4
Yyang-YE <149645095+Yyang-YE@users.noreply.github.com>
2025-02-28 14:25:08 +0900

    [Bronze V] Title: 삼각형, Time: 176 ms, Memory: 17824 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%.1f", (double) (sc.nextInt() * sc.nextInt()) / 2);
+    }
+}
