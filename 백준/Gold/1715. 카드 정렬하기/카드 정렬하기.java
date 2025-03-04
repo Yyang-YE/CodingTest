49ed78b2748be1cc9dfd15b24f5ca1fccaa3a588
Yyang-YE <149645095+Yyang-YE@users.noreply.github.com>
2025-03-04 15:38:16 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 316 ms, Memory: 25668 KB -BaekjoonHub

---
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            pq.add(Integer.parseInt(br.readLine()));
+        }
+
+        int answer = 0;
+        while(pq.size() > 1) {
+            int a = pq.poll();
+            int b = pq.poll();
+            answer += (a+b);
+            pq.add(a+b);
+        }
+        System.out.println(answer);
+    }
+}
