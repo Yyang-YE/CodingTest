4dbf061dcbc1051b5e393197d335d3cc233306e5
Yeoeun Yang <149645095+Yyang-YE@users.noreply.github.com>
2025-04-06 13:26:23 +0900

    [Bronze IV] Title: 단순한 문제 (Small), Time: 260 ms, Memory: 17184 KB -BaekjoonHub

---
+++ 백준/Bronze/25494. 단순한 문제 （Small）/단순한 문제 （Small）.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            int count = 0;
+            for (int x = 1; x <= a; x++) {
+                for (int y = 1; y <= b; y++) {
+                    for (int z = 1; z <= c; z++) {
+                        if(x % y == y % z && y % z == z % x) count++;
+                    }
+                }
+            }
+            sb.append(count).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
