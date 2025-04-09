16238e2fa2ec8dd5f9c6656be1b94ba48ade8af6
Yeoeun Yang <149645095+Yyang-YE@users.noreply.github.com>
2025-04-09 10:48:17 +0900

    [Gold III] Title: 매직 스퀘어, Time: 120 ms, Memory: 15796 KB -BaekjoonHub

--- 백준/Gold/3192. 매직 스퀘어/매직 스퀘어.java
+++ 백준/Gold/3192. 매직 스퀘어/매직 스퀘어.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -21,66 +20,65 @@ public class Main {
         }
 
         // 가운데 수 구하기
-        if(square[1][1] == 0) getMid();
+        if(square[1][1] == 0) fillMid();
 
         int max = 3 * square[1][1];
         while(!blank.isEmpty()) {
-            Coord c = blank.poll(); // 현재 좌표
+            Coord now = blank.poll(); // 현재 좌표
             // 가로 확인
             int tmp = 0;
             for (int i = 0; i < 3; i++) {
-                if(square[c.x][i] != 0) {
-                    tmp += square[c.x][i];
-                } else if(i != c.y) { // i == c.y면 상관X
+                if(square[now.x][i] != 0) {
+                    tmp += square[now.x][i];
+                } else if(i != now.y) { // 다른 0이 있으면
                     tmp = 0;
                     break;
                 }
             }
             if(tmp != 0) {
-                square[c.x][c.y] = max - tmp;
+                square[now.x][now.y] = max - tmp;
                 continue;
             }
 
-            // 세로 확인 (어차피 tmp == 0)
+            // 세로 확인 (tmp == 0)
             for (int i = 0; i < 3; i++) {
-                if(square[i][c.y] != 0) {
-                    tmp += square[i][c.y];
-                } else if(i != c.x) {
+                if(square[i][now.y] != 0) {
+                    tmp += square[i][now.y];
+                } else if(i != now.x) { // 다른 0이 있으면
                     tmp = 0;
                     break;
                 }
             }
             if(tmp != 0) {
-                square[c.x][c.y] = max - tmp;
+                square[now.x][now.y] = max - tmp;
                 continue;
             }
 
-            // 대각선 확인
+            // 대각선 확인 (tmp == 0)
             for (int i = 0; i < 3; i++) {
-                if(c.x == 1 || c.y == 1) break; // 대각선 불가 지역
-                // 좌측 대각선에 포함
-                if(c.x == c.y) {
+                if(now.x == 1 || now.y == 1) break; // 대각선 불가 지역
+                if(now.x == now.y) { // 좌측 대각선
                     if (square[i][i] != 0) {
                         tmp += square[i][i];
-                    } else if (i != c.x) { // 다른 0이 있으면
+                    } else if (i != now.x) { // 다른 0이 있으면
                         tmp = 0;
                         break;
                     }
-                } else { // 우측 대각선에 포함
+                } else { // 우측 대각선
                     if (square[i][2-i] != 0) {
                         tmp += square[i][i];
-                    } else if (i != c.x && 2-i != c.y) { // 다른 0이 있으면
+                    } else if (i != now.x && 2-i != now.y) { // 다른 0이 있으면
                         tmp = 0;
                         break;
                     }
                 }
             }
             if(tmp != 0) {
-                square[c.x][c.y] = max - tmp;
+                square[now.x][now.y] = max - tmp;
                 continue;
             }
             // 불가능하면 나중으로 미루기
-            blank.offer(c);
+            blank.offer(now);
         }
 
         for (int i = 0; i < 3; i++) {
@@ -89,16 +87,14 @@ public class Main {
             }
             bw.write("\n");
         }
-
         br.close();
         bw.close();
     }
 
-    public static void getMid() {
+    // 최대 O(4)
+    public static void fillMid() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if(i == 1 && j == 1) break;
-//                System.out.println("좌표: " + i + " " + j);
                 if(square[i][j] != 0 && square[2-i][2-j] != 0) {
                     square[1][1] = (square[i][j] + square[2-i][2-j]) / 2;
                     return;
@@ -110,7 +106,6 @@ public class Main {
     public static class Coord {
         int x;
         int y;
-        int val = 0;
 
         public Coord(int x, int y) {
             this.x = x;
