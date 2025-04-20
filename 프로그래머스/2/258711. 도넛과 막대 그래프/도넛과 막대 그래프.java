1b4f9577b090f408b3998fd5e02a7123ac218f94
Yeoeun Yang <149645095+Yyang-YE@users.noreply.github.com>
2025-04-20 20:12:56 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 32.29 ms, Memory: 202 MB -BaekjoonHub

---
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    static int N = 1000000;
+    static int[] out = new int[N+1];
+    static int[] in = new int[N+1];
+    public int[] solution(int[][] edges) {
+        int[] answer = new int[4]; // 0: 루트, 1: 도넛, 2: 막대, 3: 8자
+        
+        
+        
+        for(int[] e : edges) {
+            out[e[0]]++;
+            in[e[1]]++;
+        }
+        
+        for(int i = 1; i <= N; i++) {
+            if(in[i] == 0 && out[i] >= 2) { // root
+                answer[0] = i;
+            } else if(in[i] > 0 && out[i] == 2) { // 8자
+                answer[3]++;
+            } else if(in[i] > 0 && out[i] == 0) {
+                answer[2]++;
+            }
+        }
+        answer[1] = out[answer[0]] - answer[2] - answer[3];
+        return answer;
+    }
+}
