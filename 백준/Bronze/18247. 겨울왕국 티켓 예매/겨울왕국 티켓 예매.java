9808304f5320825c510f1c770cd2e5ae3c89b07f
Yeoeun Yang <149645095+Yyang-YE@users.noreply.github.com>
2025-05-24 16:58:00 +0900

    [Bronze III] Title: 겨울왕국 티켓 예매, Time: 152 ms, Memory: 14632 KB -BaekjoonHub

---
+++ 백준/Bronze/18247. 겨울왕국 티켓 예매/겨울왕국 티켓 예매.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 0; tc < T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+
+			if(N < 12 || M < 4) bw.write("-1\n");
+			else bw.write(M*11+4 + "\n");
+		}
+		br.close();
+		bw.close();
+	}
+}
