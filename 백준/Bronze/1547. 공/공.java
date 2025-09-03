ef17ed66f09c72f4786b9bf3768fac91cf785735
Yeoeun Yang <149645095+Yyang-YE@users.noreply.github.com>
2025-09-03 15:43:04 +0900

    [Bronze III] Title: 공, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/1547. 공/공.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int M = Integer.parseInt(br.readLine());
+        int cur = 1;
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int X = Integer.parseInt(st.nextToken());
+            int Y = Integer.parseInt(st.nextToken());
+            if(X == cur) cur = Y;
+            else if (Y == cur) cur = X;
+        }
+        System.out.println(cur);
+    }
+}
