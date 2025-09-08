75f7a8d4d49ad1c09d9260414304db1fc78478f0
Yeoeun Yang <149645095+Yyang-YE@users.noreply.github.com>
2025-09-08 17:12:34 +0900

    [Silver V] Title: 모바일 광고 입찰, Time: 444 ms, Memory: 44864 KB -BaekjoonHub

---
+++ 백준/Silver/31246. 모바일 광고 입찰/모바일 광고 입찰.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int done = 0;
+        int[] diff = new int[N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            diff[i] = B - A;
+            if(A >= B) done++;
+        }
+        Arrays.sort(diff);
+
+        if(done >= K) System.out.println(0);
+        else System.out.println(diff[K - 1]);
+    }
+}
