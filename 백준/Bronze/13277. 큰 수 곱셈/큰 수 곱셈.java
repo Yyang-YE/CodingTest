2126a45ddd48eddf6ad44d0d57bc6507188315c7
Yeoeun Yang <149645095+Yyang-YE@users.noreply.github.com>
2025-10-03 14:16:19 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 5608 ms, Memory: 406804 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] num = br.readLine().split(" ");
+		BigInteger A = new BigInteger(num[0]);
+		BigInteger B = new BigInteger(num[1]);
+		System.out.println(A.multiply(B));
+	}
+}
