c3676e72cc50e4985f895e9b88cc2dfdca4287fd
Yeoeun Yang <149645095+Yyang-YE@users.noreply.github.com>
2025-10-17 16:35:27 +0900

    [Gold V] Title: 평범한 배낭, Time: 160 ms, Memory: 53820 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,47 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
 
-        Item[] items = new Item[N];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            items[i] = new Item(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-        }
+		// Item
+		Item[] items = new Item[N+1];
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			items[i] = new Item(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+		}
 
-        int[][] dp = new int[N][K+1];
-        for (int i = 0; i < N; i++) { // item 개수
-            for (int j = 1; j <= K; j++) { // 무게
-                if(i==0) {
-                    if(j < items[i].W) dp[i][j] = 0;
-                    else dp[i][j] = items[i].V;
-                } else {
-                    if (j >= items[i].W) {
-                        dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - items[i].W] + items[i].V);
-                    } else {
-                        dp[i][j] = dp[i-1][j];
-                    }
-                }
-            }
-        }
-        System.out.println(dp[N-1][K]);
-    }
+		// N 까지의 아이템으로 K보다 작은 무게로 만들 수 있는 최대 가치
+		int[][] dp = new int[N+1][K+1];
 
-    public static class Item {
-        int W;
-        int V;
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= K; j++) {
+				if(items[i].w > j) { // i번째 무게를 담을 수 없음
+					dp[i][j] = dp[i-1][j]; // i-1번째까지의 최대로 설정
+				} else { // i번째 담을 수 있음
+					// j-w의 최대에서 현재 것을 담았을 경우
+					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j - items[i].w] + items[i].v);
+				}
+			}
+		}
+		System.out.println(dp[N][K]);
+	}
 
-        public Item(int W, int V) {
-            this.W = W;
-            this.V = V;
-        }
-    }
+	public static class Item {
+		int w; // 무게
+		int v; // 가치
+
+		public Item(int w, int v) {
+			this.w = w;
+			this.v = v;
+		}
+	}
 }
