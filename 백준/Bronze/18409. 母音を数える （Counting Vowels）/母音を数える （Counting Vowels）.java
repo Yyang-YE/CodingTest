fd1d26e5408134e243f989d0fc66c3476e2a8a71
Yeoeun Yang <149645095+Yyang-YE@users.noreply.github.com>
2025-10-19 21:50:48 +0900

    [Bronze V] Title: 母音を数える (Counting Vowels), Time: 100 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/18409. 母音を数える （Counting Vowels）/母音を数える （Counting Vowels）.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            char c = str.charAt(i);
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
