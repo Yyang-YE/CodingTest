6da4ed8282bd1ca5c6fd771a2920c378d6fd9381
Yeoeun Yang <149645095+Yyang-YE@users.noreply.github.com>
2025-10-22 16:21:28 +0900

    [Bronze IV] Title: MBTI, Time: 104 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/25640. MBTI/MBTI.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String jinho = br.readLine();
+        int N = Integer.parseInt(br.readLine());
+
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            if(jinho.equals(br.readLine())) count++;
+        }
+        System.out.println(count);
+    }
+}
