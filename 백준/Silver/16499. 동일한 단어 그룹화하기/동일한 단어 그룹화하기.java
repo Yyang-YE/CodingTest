de632a7019b9e1de5c147c00f39d3b26b4299829
Yeoeun Yang <149645095+Yyang-YE@users.noreply.github.com>
2025-10-30 19:18:48 +0900

    [Silver IV] Title: 동일한 단어 그룹화하기, Time: 100 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Silver/16499. 동일한 단어 그룹화하기/동일한 단어 그룹화하기.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            char[] arr = br.readLine().toCharArray();
+            Arrays.sort(arr);
+            set.add(String.valueOf(arr));
+        }
+        System.out.println(set.size());
+    }
+}
